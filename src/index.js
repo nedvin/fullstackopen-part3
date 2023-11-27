9340553c5f0b51732f3d8e50964f58aff2451737
nedvin <niemi.edvin@outlook.com>
2023-11-27 15:03:50 +0100

    exercises 3.15-3.18

--- src/index.js
+++ src/index.js
@@ -34,11 +34,13 @@ app.use(
   })
 );
 
-app.get("/api/persons", (request, response) => {
-  Person.find({}).then((result) => response.json(result));
+app.get("/api/persons", (request, response, next) => {
+  Person.find({})
+    .then((result) => response.json(result))
+    .catch((error) => next(error));
 });
 
-app.post("/api/persons", (request, response) => {
+app.post("/api/persons", (request, response, next) => {
   const { name, number } = { ...request.body };
 
   if (!name) {
@@ -50,35 +52,63 @@ app.post("/api/persons", (request, response) => {
   }
 
   const person = new Person({ name, number });
-  person.save().then((person) => response.status(201).json(person));
+  person
+    .save()
+    .then((person) => response.status(201).json(person))
+    .catch((error) => next(error));
 });
 
-app.get("/api/persons/:id", (request, response) => {
-  Person.findById(request.params.id).then((person) => {
-    if (person === null) {
-      return response.status(404).end();
-    }
+app.get("/api/persons/:id", (request, response, next) => {
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (person === null) {
+        return response.status(404).end();
+      }
 
-    response.json(person);
-  });
+      response.json(person);
+    })
+    .catch((error) => next(error));
 });
 
-app.delete("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  persons = persons.filter((person) => person.id !== id);
-
-  response.status(204).end();
+app.delete("/api/persons/:id", (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then((result) => response.status(204).end())
+    .catch((error) => next(error));
 });
 
-app.get("/info", (request, response) => {
-  const numberOfPersons = persons.length;
-  const time = new Date();
-
-  response.send(`
-    <p>Phonebook has info for ${numberOfPersons} people</p>
-    <p>${time}</p>
-    `);
+app.put("/api/persons/:id", (request, response, next) => {
+  const { name, number } = { ...request.body };
+  Person.findByIdAndUpdate(request.params.id, { name, number }, { new: true })
+    .then((result) => {
+      response.json(result);
+    })
+    .catch((error) => next(error));
 });
+
+app.get("/info", (request, response, next) => {
+  Person.find({})
+    .then((result) => {
+      const numberOfPersons = result.length;
+      const time = new Date();
+      response.send(`
+      <p>Phonebook has info for ${numberOfPersons} people</p>
+      <p>${time}</p>
+      `);
+    })
+    .catch((error) => next(error));
+});
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+
+  next(error);
+};
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`app is now listening on port ${PORT}`));
